fac562f27fd97c106d397c243200c059d3733e02
NemanjaVasiljevic <nemus.vasiljevic@gmail.com>
2023-06-23 12:10:06 +0200

    odradjeno slanje byte po byte i provera duzine poruke

--- Client.c
+++ Client.c
@@ -9,11 +9,14 @@
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8888
 #define BUFFER_SIZE 1024
+#define MAX_MESSAGE_LEN 20
+
+int SendMessageLength(int* length, int sockfd, struct sockaddr_in server_addr);
 
 
 typedef struct{
-    int messageNum;
-    char message[BUFFER_SIZE];
+    int messageLength;
+    char message[MAX_MESSAGE_LEN];
 }Data;
 
 
@@ -21,6 +24,9 @@ static int messageCounter = 0;
 
 
 int main() {
+
+#pragma region SOCKET_SETUP     
+    
     int sockfd;
     struct sockaddr_in server_addr;
 
@@ -49,62 +55,154 @@ int main() {
         exit(1);
     }
 
+#pragma endregion
     // Send data to the server
 
     while(1){
 
         Data* data = (Data*)malloc(sizeof(Data));
 
-        data->messageNum = ++messageCounter;
+
         printf("Enter message: ");
         scanf("%s",data->message);
-        //printf("Uneta poruka %s", data->message);
+        char byte;
         char* buffer = (char*)malloc(sizeof(Data));
         memcpy(buffer, data, sizeof(Data));
 
-        if (sendto(sockfd, buffer, sizeof(Data), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-            perror("sendto");
-            exit(1);
-        }
+        size_t messageLen = strlen(data->message);
+        size_t bytesSent = 0;
+        
+        if(messageLen > MAX_MESSAGE_LEN){
 
-        // Use select() for non-blocking receive
-        fd_set read_fds;
-        FD_ZERO(&read_fds);
-        FD_SET(sockfd, &read_fds);
+            printf("The message length was exceeded please write something shorter\n");
 
-        struct timeval timeout;
-        timeout.tv_sec = 5;  // Set a timeout of 5 seconds
-        timeout.tv_usec = 0;
+        }else{
 
-        int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
-        if (select_result == -1) {
-            perror("select");
-            exit(1);
-        } else if (select_result == 0) {
 
-            printf("Timeout reached. Resending message...\n");
-            
+            data->messageLength = messageLen;                      
+            if(SendMessageLength((int *)&messageLen,sockfd,server_addr)!=1){
 
-        } else {
-            if (FD_ISSET(sockfd, &read_fds)) {
-                // Receive response from the server
-                memset(buffer, 0, BUFFER_SIZE);
-                socklen_t server_addr_len = sizeof(server_addr);
-                if (recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &server_addr_len) == -1) {
-                    perror("recvfrom");
-                    exit(1);
+                printf("The message length is too great for the server to accept\n");
+
+            }else{
+                printf("Upao u while za slanje\n");
+
+
+                while(bytesSent<messageLen){
+                    printf("Upao u while za slanje bajtova\n");
+
+                    byte = data->message[bytesSent];
+
+                    if (sendto(sockfd, &byte, sizeof(char), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+                        perror("sendto");
+                        exit(1);
+                    }
+
+                    // Use select() for non-blocking receive
+                    fd_set read_fds;
+                    FD_ZERO(&read_fds);
+                    FD_SET(sockfd, &read_fds);
+
+                    struct timeval timeout;
+                    timeout.tv_sec = 5;  // Set a timeout of 5 seconds
+                    timeout.tv_usec = 0;
+
+                    int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+
+
+                    if (select_result == -1) {
+
+                        perror("select");
+                        exit(1);
+
+                    } else if (select_result == 0) {
+
+                        printf("Timeout reached. Resending message...\n");
+                        /*Implementirati ponovno slanje*/
+
+                    } else {
+                        if (FD_ISSET(sockfd, &read_fds)) {
+                            char ackByte;
+                            socklen_t server_addr_len = sizeof(server_addr);
+
+                            if (recvfrom(sockfd, &ackByte, sizeof(char), 0, (struct sockaddr *)&server_addr, &server_addr_len) == -1) {
+                                perror("recvfrom");
+                                exit(1);
+                            }
+
+                            //Ispis ACK prijema
+                            printf("Received: %c\n", ackByte);
+                            if(ackByte == byte){
+                                bytesSent++;
+                            }
+                        }
+
+                    }
                 }
-
-                // Print the echoed message from the server
-                printf("Received: %s\n", buffer);
-            }
-
-            free(buffer);
-            free(data);
+            }         
         }
+
+        free(buffer);
+        free(data);
     }
     // Close the socket
     close(sockfd);
 
     return 0;
+}
+
+
+
+int SendMessageLength(int* length, int sockfd, struct sockaddr_in server_addr){
+
+    int ackByte;
+
+
+    if (sendto(sockfd, length, sizeof(int), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+                    perror("sendto");
+                    exit(1);
+    }
+
+    // Use select() for non-blocking receive
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(sockfd, &read_fds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 5;  // Set a timeout of 5 seconds
+    timeout.tv_usec = 0;
+
+    int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+
+
+    if (select_result == -1) {
+
+        perror("select");
+        return 0;
+
+    } else if (select_result == 0) {
+
+        printf("Timeout reached. Resending message...\n");
+        return 0;
+
+    } else {
+        if (FD_ISSET(sockfd, &read_fds)) {
+            socklen_t server_addr_len = sizeof(server_addr);
+
+            if (recvfrom(sockfd, &ackByte, sizeof(int), 0, (struct sockaddr *)&server_addr, &server_addr_len) == -1) {
+                perror("recvfrom");
+                return 0;
+            }
+
+            //Ispis ACK prijema
+            printf("Received: %d\n", ackByte);
+        }
+        
+    }
+
+    if(ackByte!=0){
+        return 1;
+    }else{
+        return 0;
+    }
 }
--- Server.c
+++ Server.c
@@ -8,14 +8,21 @@
 
 #define SERVER_PORT 8888
 #define BUFFER_SIZE 1024
+#define MAX_MESSAGE_LEN 20
 
 typedef struct{
     int messageNum;
-    char message[BUFFER_SIZE];
+    char message[MAX_MESSAGE_LEN];
 }Data;
 
+int CheckMessageLength(int sockfd, int* length, struct sockaddr_in client_addr);
+int checked = 1;
+int byteCounter=0;
 
 int main() {
+
+#pragma region SOCKET_SETUP
+
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
 
@@ -49,7 +56,13 @@ int main() {
         exit(1);
     }
 
+#pragma endregion
+
     while (1) {
+
+        char byte;
+        int messageLength;
+
         // Use select() for non-blocking receive
         char* buffer = (char*)malloc(sizeof(Data));
         Data* data = (Data*)malloc(sizeof(Data));
@@ -63,31 +76,50 @@ int main() {
         timeout.tv_usec = 0;
 
         int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+
         if (select_result == -1) {
             perror("select");
             exit(1);
+
         } else if (select_result == 0) {
             printf("No data received.\n");
+            checked = 1;
+
         } else {
             if (FD_ISSET(sockfd, &read_fds)) {
-                memset(buffer, 0, BUFFER_SIZE);
-                socklen_t client_addr_len = sizeof(client_addr);
-
-                // Receive data from a client
-                if (recvfrom(sockfd, buffer, sizeof(Data), 0, (struct sockaddr *)&client_addr, &client_addr_len) == -1) {
-                    perror("recvfrom");
-                    exit(1);
-                }
                 
-                memcpy(data,buffer,sizeof(Data));
-
-                printf("Received from client: %d %s\n", data->messageNum, data->message);
-
-                // Echo the received message back to the client
-                if (sendto(sockfd, "ACK", 4, 0, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
-                    perror("sendto");
-                    exit(1);
+                socklen_t client_addr_len = sizeof(client_addr);
+                int length=0;
+                if(checked == 1){
+                    messageLength = CheckMessageLength(sockfd,&length,client_addr);
                 }
+                if(messageLength!=0){
+                    checked = 0;
+                    //printf("Dobio poruku koju mogu da primim sad radim primanje bajta po bajt\n");
+                    sleep(1);
+
+                    // Receive data from a client
+                    if (recvfrom(sockfd, &byte, sizeof(char), 0, (struct sockaddr *)&client_addr, &client_addr_len) == -1) {
+                        perror("recvfrom");
+                        exit(1);
+                    }
+                    
+                    if(byteCounter < messageLength-1){
+                        printf("Received Byte: %c\n", byte);
+                        data->message[byteCounter] = byte;
+                        byteCounter++;
+                    }else{
+                        printf("Finished sending full message is: %s\n",data->message);
+                        byteCounter=0;
+                    }
+
+                    // Echo the received message back to the client
+                    if (sendto(sockfd, &byte, sizeof(char), 0, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
+                        perror("sendto");
+                        exit(1);
+                    }
+                }
+
             }
         }
 
@@ -99,4 +131,54 @@ int main() {
     close(sockfd);
 
     return 0;
+}
+
+
+int CheckMessageLength(int sockfd, int* length, struct sockaddr_in client_addr){
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(sockfd, &read_fds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 1;  // Set a timeout of 1 second
+    timeout.tv_usec = 0;
+
+    int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &timeout);
+
+    if (select_result == -1) {
+        perror("select");
+        return 0;
+
+    } else if (select_result == 0) {
+        printf("No data received.\n");
+        return 0;
+
+    } else {
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        if (recvfrom(sockfd, length, sizeof(int), 0, (struct sockaddr *)&client_addr, &client_addr_len) == -1) {
+            perror("recvfrom");
+            return 0;
+        }
+
+        printf("Received length: %d\n", *length);
+
+        if(*length < MAX_MESSAGE_LEN){
+
+            // Echo the received message back to the client
+            if (sendto(sockfd, length, sizeof(int), 0, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
+                perror("sendto");
+                return 0;
+            }
+            return *length;
+        }else{
+
+            // Echo the received message back to the client
+            if (sendto(sockfd, 0, sizeof(int), 0, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
+                perror("sendto");
+                return 0;
+            }
+            return 0;
+        }
+    }
 }
